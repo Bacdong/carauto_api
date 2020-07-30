d93ec705ef63ad67fbb417321624b5440ee478b1
Bacdong <duongdong2203@gmai.com>
2020-07-30 19:11:35 +0700

    Fix settings.py

--- carauto/carauto/settings.py
+++ carauto/carauto/settings.py
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'vi-vn'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
 USE_I18N = True
 
